8be5eca60422dc046926d818db24adb4d4c8620d
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2023-03-16 00:00:03 +1100

    Try different indexes

--- notionbot/notionbot.py
+++ notionbot/notionbot.py
@@ -1,9 +1,15 @@
 import os
+from typing import Type
 import fire
 import requests
 from pathlib import Path
 import coloredlogs
-from llama_index import GPTSimpleVectorIndex, NotionPageReader
+from llama_index import (
+    GPTListIndex,
+    GPTSimpleVectorIndex,
+    GPTTreeIndex,
+    NotionPageReader,
+)
 from llama_index.readers import notion
 from llama_index.readers.schema.base import Document
 from llama_index import LLMPredictor, GPTSimpleVectorIndex
@@ -12,7 +18,7 @@ from langchain import OpenAI
 from langchain.agents import Tool, initialize_agent
 from langchain.chains.conversation.memory import ConversationBufferMemory
 
-coloredlogs.install(level="INFO")
+coloredlogs.install(level="DEBUG")
 
 
 def main(database_id: str):
@@ -27,7 +33,8 @@ def main(database_id: str):
     llm = OpenAI(
         temperature=0, model_name="gpt-3.5-turbo"
     )  # type: ignore  # defauts to text-davinci-003
-    index = build_index_for_notion_db(database_id, llm=llm)
+    index = build_index_for_notion_db(database_id, index_cls=GPTListIndex, llm=llm)
+    # index = build_index_for_notion_db(database_id, GPTTreeIndex, llm=llm)
 
     # Tools that the AI (agent) can use if needed
     when_to_use_the_tool = """
@@ -56,16 +63,20 @@ def main(database_id: str):
         print(response)
 
 
-def build_index_for_notion_db(database_id: str, llm=None) -> BaseGPTIndex:
+def build_index_for_notion_db(
+    database_id: str, index_cls: Type = GPTSimpleVectorIndex, llm=None
+) -> BaseGPTIndex:
     """
     Build a Llama-index from a Notion database {database_id}
     """
-    save_path = Path(__file__).parent / "saves" / f"{database_id}.json"
+    save_path = (
+        Path(__file__).parent / "saves" / f"{database_id}_{index_cls.__name__}.json"
+    )
     if save_path.exists():
         print(f"Loading index from {save_path}")
-        return GPTSimpleVectorIndex.load_from_disk(str(save_path))
+        return index_cls.load_from_disk(str(save_path))
 
-    print(f"Loading data from the database {database_id}")
+    print(f"Loading data from the database {database_id}, will write to {save_path}")
     loader = NotionPageReader(os.environ["NOTION_TOKEN"])
     cursor = None
     page_num = 0
@@ -93,7 +104,7 @@ def build_index_for_notion_db(database_id: str, llm=None) -> BaseGPTIndex:
             break
 
     print("Indexing documents")
-    index = GPTSimpleVectorIndex(docs, llm_predictor=LLMPredictor(llm) if llm else None)
+    index = index_cls(docs, llm_predictor=LLMPredictor(llm) if llm else None)
     index.save_to_disk(str(save_path))
     return index
 
